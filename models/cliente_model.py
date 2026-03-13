382c87d65c406e313d4f0432efd6ff85dbbc11e3
Josubat <josuejimenezrocha5@gmail.com>
2026-03-13 10:58:26 -0500

    Add files via upload
    ya ahora si completo

---
+++ models/cliente_model.py
@@ -0,0 +1,9 @@
+class cliente:
+
+    def __init__(self, CLI_ID, CLI_NOMBRE, CLI_APELLIDO, CLI_TELEFONO, CLI_DIRECCION, CLI_CORREO):
+        self.Cli_id = CLI_ID
+        self.Cli_nombre = CLI_NOMBRE
+        self.Cli_apellido = CLI_APELLIDO
+        self.Cli_telefono = CLI_TELEFONO
+        self.Cli_direccion = CLI_DIRECCION
+        self.Cli_correo = CLI_CORREO
---
+++ models/detalles_de_pedido_model.py
@@ -0,0 +1,6 @@
+class detalle_de_pedido:
+    def __init__(self, ID, CATIDAD, SUBTOTAL):
+        self.Det_id = ID
+        self.Det_cantidad = CATIDAD
+        self.Det_subtotal = SUBTOTAL
+        
