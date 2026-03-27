941f810300e20625be7e636d45aa2b2a1d374b73
zzskrillexzz <josecarlosfernandez05@gmail.com>
2026-03-26 23:41:32 -0500

    inventario en movimiento y  provedor ya quedo

--- routers/__init__.py
+++ routers/__init__.py
@@ -3,7 +3,8 @@ from .pedido import pedido_bp
 from .factura import facturas_bp
 from .producto import productos_bp
 from .detalle_de_pedido_route import detalles_bp
-#from .proveedor import proveedores_bp
+from .proveedor import proveedores_bp
+from .inventario_movimiento import inventario_bp
 #from .usuario import usuarios_bp
 
 def cargarruta(app):
@@ -12,5 +13,6 @@ def cargarruta(app):
     app.register_blueprint(facturas_bp, url_prefix='/facturas')
     app.register_blueprint(productos_bp, url_prefix='/productos')
     app.register_blueprint(detalles_bp, url_prefix='/detalles')
-#    app.register_blueprint(proveedores_bp, url_prefix='/proveedores')
+    app.register_blueprint(proveedores_bp, url_prefix='/proveedores')
+    app.register_blueprint(inventario_bp, url_prefix='/inventario')
 #    app.register_blueprint(usuarios_bp, url_prefix='/usuarios')
